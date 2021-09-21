fb9d7705aa74fdeb7bdbf3b9502d24882cb3d2a1
DiogoM14 <diogomartins200214@gmail.com>
2021-09-21 22:54:52 +0100

    Infinite query config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Button, Box } from '@chakra-ui/react';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
 import { Header } from '../components/Header';
@@ -16,12 +16,18 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images',
+    async ({ pageParam = null }) => {
+      if (pageParam !== null)
+        return await api.get('/images', {
+          params: {
+            after: pageParam,
+          },
+        });
+      return await api.get('/images');
+    }, {
+      getNextPageParam: (lastPage, page) => lastPage.data.after
+    });
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
@@ -36,7 +42,7 @@ export default function Home(): JSX.Element {
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData} />
+        {/* <CardList cards={formattedData} /> */}
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
       </Box>
     </>
